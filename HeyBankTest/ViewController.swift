7b6700d060543b4cd9bc4d0dc5e749f2d9738727
AmericoMQ <beatamez@hotmail.com>
2024-10-22 18:35:59 -0600

    initializing repo

---
+++ HeyBankTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeyBankTest
+//
+//  Created by Americo Meneses on 22/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
